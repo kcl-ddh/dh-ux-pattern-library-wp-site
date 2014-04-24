4bf509872e0ddc22b79a91a925cc903bcc5b4cd3
David Little <david@littled.net>
2014-04-24 16:00:12 +0100

    Files copied back in
    Blah

---
+++ footer.php
@@ -0,0 +1,64 @@
+	</div>
+</section>
+
+<footer class="footer-section">
+	<div class="row">
+		<div class="large-4 columns">
+			<ul class="footer-links">
+				<li><h3>Links</h3></li>
+				<li><a href="http://www.kcl.ac.uk/ddh/">Department of Digital Humanities</a></li>
+				<li><a href="http://wip.cch.kcl.ac.uk">Digital Humanities: Works in Progress</a><li>
+			</ul>
+			
+			<ul class="footer-links">
+				<li><h3>Inspiration</h3></li>
+				<li><a href="http://www.bbc.co.uk/gel">BBC GEL</a></li>
+				<li><a href="http://www.jnd.org/">Don Norman</a></li>
+				<li><a href="http://ui-patterns.com">UI Patterns</a></li>
+				<li><a href="http://developer.yahoo.com/ypatterns/">Yahoo Design Pattern Library</a></li>
+				<li><a href="http://foundation.zurb.com">Zurb Foundation</a></li>
+
+			</ul>
+		</div>
+		<div class="large-4 columns">
+			<ul class="footer-links">
+				<li><h3>Example projects</h3></li>
+				<li><a href="http://www.artofmaking.ac.uk/">Art of Making</a></li>
+				<li><a href="http://www.crsbi.ac.uk/">CRSBI</a><li>
+				<li><a href="http://discover.medievalchester.ac.uk">Discover Medieval Chester</a></li>
+				<li><a href="http://www.goughmap.org/">Linguistic Geographies</a></li>
+			</ul>
+		</div>
+		<div class="large-2 columns">
+			<ul class="footer-links">
+				<li><h3>About</h3></li>
+				<li><a href="/contact/">Contact</a></li>
+				<li><a href="/about/privacy/">Privacy</a><li>
+			</ul>
+		</div>
+		<div class="large-2 columns large">
+		<!-- <?php do_action('foundationPress_before_footer'); ?>
+		<?php dynamic_sidebar("footer-widgets"); ?>
+		<?php do_action('foundationPress_after_footer'); ?>
+		-->
+			
+			<ul class="footer-links">
+				<li><a href="http://www.kcl.ac.uk/ddh/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/kcl-ddh-logo.png" alt="Department of Digital Humanities, King's College London"></a></li>
+			</ul>
+			
+			
+		</div>
+		
+	</div>
+</footer>
+
+
+<a class="exit-off-canvas"></a>
+	
+  <?php do_action('foundationPress_layout_end'); ?>
+  </div>
+</div>
+<?php wp_footer(); ?>
+<?php do_action('foundationPress_before_closing_body'); ?>
+</body>
+</html>
